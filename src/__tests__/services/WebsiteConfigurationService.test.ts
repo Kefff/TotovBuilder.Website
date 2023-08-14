7ccdee8e9f817267c6c6c86c858c57b5bf340f85
Kefff <1677411+Kefff@users.noreply.github.com>
2023-08-14 19:27:58 +0200

    Added unit test.

--- src/__tests__/services/WebsiteConfigurationService.test.ts
+++ src/__tests__/services/WebsiteConfigurationService.test.ts
@@ -3,10 +3,10 @@ import { WebsiteConfigurationService } from '../../services/WebsiteConfiguration
 import WebsiteConfigurationMock from '../../../test-data/website-configuration.json'
 import { useApiServiceMock } from '../../__mocks__/ApiServiceMock'
 import { ApiService } from '../../services/ApiService'
-import { anyString, instance, mock, when } from 'ts-mockito'
+import { anyString, anything, instance, mock, spy, verify, when } from 'ts-mockito'
 import Result, { FailureType } from '../../utils/Result'
 import Services from '../../services/repository/Services'
-import { NotificationService } from '../../services/NotificationService'
+import { NotificationService, NotificationType } from '../../services/NotificationService'
 import { useWebsiteConfigurationServiceMock } from '../../__mocks__/WebsiteConfigurationServiceMock'
 import { describe, expect, it } from 'vitest'
 import { ServiceInitializationState } from '../../services/repository/ServiceInitializationState'
@@ -24,6 +24,60 @@ describe('initialize', () => {
     // Assert
     expect(result).toBe(true)
     expect(service.configuration.buildSharingUrl).toBe('localhost:3000/s/')
+  })
+
+  it('should notify when the post update period flag is set', async () => {
+    // Arrange
+    useApiServiceMock({
+      allowCookiesStorageKey: "allow_cookies",
+      bugReportUrl: "https://discord.gg/bugreport",
+      buildSharingUrl: "localhost:3000/s/",
+      buildsSortFieldStorageKey: "builds_sort_field",
+      buildsSortOrderStorageKey: "builds_sort_order",
+      buildStorageKeyPrefix: "build_",
+      cacheDuration: 3600,
+      changelogApi: "changelog",
+      contactAddress: "contact@address.com",
+      discordUrl: "https://discord.gg/server",
+      exportFileExtension: ".ttb",
+      exportFileNamePrefix: "TotovBuilder",
+      exportWarningShowedStoregeKey: "export_warning",
+      fetchMaxTries: 3,
+      fetchTimeout: 30,
+      fetchWaitTimeBetweenRetries: 2,
+      githubUrl: "https://github.com/User/Reposiory",
+      globalFilterStorageKey: "global_filter",
+      itemCategoriesApi: "itemcategories",
+      itemsApi: "items",
+      languageStorageKey: "language",
+      notificationErrorDuration: 10,
+      notificationInformationDuration: 5,
+      notificationSuccessDuration: 5,
+      notificationWarningDuration: 10,
+      postUpdatePeriod: true,
+      presetsApi: "presets",
+      pricesApi: "prices",
+      questsApi: "quests",
+      tarkovValuesApi: "tarkovvalues",
+      version: "1.6.0",
+      versionStorageKey: "version"
+    } as IWebsiteConfiguration)
+    Services.configure(NotificationService)
+
+    const notificationServiceSpy = spy(Services.get(NotificationService))
+
+    const service = new WebsiteConfigurationService()
+
+    // Act
+    const result = await service.initialize()
+
+    // Assert
+    expect(result).toBe(true)
+    verify(notificationServiceSpy.notify(
+      NotificationType.information,
+      'As we are in a post Tarkov update period, items and prices may not be up to date yet.\nThey are progressively being updated by the Tarkov.dev team.',
+      true,
+      0)).once()
   })
 
   it('should do nothing when the API returns an error', async () => {
