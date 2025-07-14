873916077a70fe54e1871b7261c70737d0e4d503
Kefff <1677411+Kefff@users.noreply.github.com>
2025-07-14 10:54:15 +0200

    Added unit test.

--- src/__tests__/services/sorting/functions/itemSortingFunction.Item.test.ts
+++ src/__tests__/services/sorting/functions/itemSortingFunction.Item.test.ts
@@ -1,6 +1,7 @@
 import { anything, instance, mock, when } from 'ts-mockito'
 import { describe, expect, it } from 'vitest'
 import { IInventoryItem } from '../../../../models/build/IInventoryItem'
+import { IBarterItem } from '../../../../models/item/IBarterItem'
 import { IItem, ItemCategoryId } from '../../../../models/item/IItem'
 import { IPrice } from '../../../../models/item/IPrice'
 import { IInventoryItemPrice } from '../../../../models/utils/IInventoryItemPrice'
@@ -11,6 +12,10 @@ import { InventoryItemService } from '../../../../services/InventoryItemService'
 import Services from '../../../../services/repository/Services'
 import { SortingService } from '../../../../services/sorting/SortingService'
 import { ItemSortingFunctions } from '../../../../services/sorting/functions/itemSortingFunctions'
+import { mts25512wood } from '../../../__data__/itemMocks'
+import { useItemServiceMock } from '../../../__mocks__/ItemServiceMock'
+import { useTarkovValuesServiceMock } from '../../../__mocks__/TarkovValuesServiceMock'
+import { useWebsiteConfigurationServiceMock } from '../../../__mocks__/WebsiteConfigurationServiceMock'
 
 describe('comparisonFunction', () => {
   it.each([
@@ -182,6 +187,195 @@ describe('comparisonFunction', () => {
 
     // Assert
     expect(sortedItems).toStrictEqual([item2, item1, item3]) // By default it is already sorted by name, so here we expected items to be sorted in a descending way
+  })
+
+  it('should sort by price and group barters with missing prices and prices with a currency without value', async () => {
+    // Arrange
+    const itemWithMissingBarterPrices1 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithMissingBarterPrices1',
+      name: 'itemWithMissingBarterPrices1'
+    } as IItem
+
+    const itemWithMissingBarterPrices2 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithMissingBarterPrices2',
+      name: 'itemWithMissingBarterPrices2'
+    } as IItem
+
+    const itemWithCurrencyWithoutValue1_1 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithCurrencyWithoutValue1_1',
+      name: 'itemWithCurrencyWithoutValue1_1'
+    } as IItem
+
+    const itemWithCurrencyWithoutValue1_2 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithCurrencyWithoutValue1_2',
+      name: 'itemWithCurrencyWithoutValue1_2'
+    } as IItem
+
+    const itemWithCurrencyWithoutValue2_1 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithCurrencyWithoutValue2_1',
+      name: 'itemWithCurrencyWithoutValue2_1'
+    } as IItem
+
+    const itemWithCurrencyWithoutValue2_2 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithCurrencyWithoutValue2_2',
+      name: 'itemWithCurrencyWithoutValue2_2'
+    } as IItem
+
+    const itemWithPrice1 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithPrice1',
+      name: 'itemWithPrice1'
+    } as IItem
+
+    const itemWithPrice2 = {
+      categoryId: ItemCategoryId.armor,
+      id: 'itemWithPrice2',
+      name: 'itemWithPrice2'
+    } as IItem
+
+    useWebsiteConfigurationServiceMock()
+    useTarkovValuesServiceMock()
+    useItemServiceMock(
+      true,
+      [
+        itemWithCurrencyWithoutValue1_1,
+        itemWithCurrencyWithoutValue1_2,
+        itemWithCurrencyWithoutValue2_1,
+        itemWithCurrencyWithoutValue2_2,
+        itemWithMissingBarterPrices1,
+        itemWithMissingBarterPrices2,
+        itemWithPrice1,
+        itemWithPrice2
+      ],
+      [
+        {
+          barterItems: [
+            {
+              itemId: mts25512wood.id,
+              quantity: 1
+            }
+          ],
+          currencyName: 'barter',
+          itemId: itemWithMissingBarterPrices1.id,
+          merchant: 'prapor',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 0,
+          valueInMainCurrency: 0
+        },
+        {
+          barterItems: [
+            {
+              itemId: mts25512wood.id,
+              quantity: 1
+            }
+          ],
+          currencyName: 'barter',
+          itemId: itemWithMissingBarterPrices2.id,
+          merchant: 'prapor',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 0,
+          valueInMainCurrency: 0
+        },
+        {
+          barterItems: [] as IBarterItem[],
+          currencyName: 'GPCOIN',
+          itemId: itemWithCurrencyWithoutValue1_1.id,
+          merchant: 'ref',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 6,
+          valueInMainCurrency: 0
+        },
+        {
+          barterItems: [] as IBarterItem[],
+          currencyName: 'GPCOIN',
+          itemId: itemWithCurrencyWithoutValue1_2.id,
+          merchant: 'ref',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 12,
+          valueInMainCurrency: 0
+        },
+        {
+          barterItems: [] as IBarterItem[],
+          currencyName: 'TOTOCOIN',
+          itemId: itemWithCurrencyWithoutValue2_1.id,
+          merchant: 'ref',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 5,
+          valueInMainCurrency: 0
+        },
+        {
+          barterItems: [] as IBarterItem[],
+          currencyName: 'TOTOCOIN',
+          itemId: itemWithCurrencyWithoutValue2_2.id,
+          merchant: 'ref',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 10,
+          valueInMainCurrency: 0
+        },
+        {
+          barterItems: [] as IBarterItem[],
+          currencyName: 'RUB',
+          itemId: itemWithPrice1.id,
+          merchant: 'ragman',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 10000,
+          valueInMainCurrency: 10000
+        },
+        {
+          barterItems: [] as IBarterItem[],
+          currencyName: 'RUB',
+          itemId: itemWithPrice2.id,
+          merchant: 'ragman',
+          merchantLevel: 1,
+          quest: undefined,
+          value: 20000,
+          valueInMainCurrency: 20000
+        }
+      ])
+
+    const sortingData = new ItemFilterAndSortingData(ItemSortingFunctions)
+    sortingData.property = 'price'
+
+    const sortingService = new SortingService()
+
+    // Act
+    const sortedItems = await sortingService.sortAsync(
+      [
+        itemWithCurrencyWithoutValue2_1,
+        itemWithPrice2,
+        itemWithCurrencyWithoutValue2_2,
+        itemWithMissingBarterPrices2,
+        itemWithCurrencyWithoutValue1_2,
+        itemWithPrice1,
+        itemWithCurrencyWithoutValue1_1,
+        itemWithMissingBarterPrices1
+      ],
+      sortingData!)
+
+    // Assert
+    expect(sortedItems).toStrictEqual([
+      itemWithMissingBarterPrices1,
+      itemWithMissingBarterPrices2,
+      itemWithCurrencyWithoutValue1_1,
+      itemWithCurrencyWithoutValue1_2,
+      itemWithCurrencyWithoutValue2_1,
+      itemWithCurrencyWithoutValue2_2,
+      itemWithPrice1,
+      itemWithPrice2
+    ])
   })
 
   it('should sort by weight', async () => {
